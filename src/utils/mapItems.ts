2ca6ebf4ff6928d082ebc231e2f53211212e1188
Ihor Horb <gorbigorolegovich@gmail.com>
2022-10-21 13:49:10 +0300

    mapItems function is optimized.

--- src/utils/mapItems.ts
+++ src/utils/mapItems.ts
@@ -1,14 +1,20 @@
 import {ItemInterface} from "../types/item.interface";
 
-export const mapItems = (payload: ItemInterface[]):ItemInterface[] => {
-    const duplicatedIds: Array<number> = []
-    return payload.map((item) => {
-        for (let i = 0; i < payload.length; i++) {
-            if (item.id === payload[i].parent_id) {
-                item.children = item.children ? [...item.children, payload[i]] : [payload[i]]
-                duplicatedIds.push(payload[i].id)
-            }
+export const mapItems = (payload: ItemInterface[]): ItemInterface[] => {
+
+    const result: ItemInterface[] = []
+    const payloadCopy: ItemInterface[] = JSON.parse(JSON.stringify(payload))
+
+    payloadCopy.forEach((item, index, array) => {
+        if (item.parent_id === 0) {
+            result.push(item)
+        } else if (item.parent_id > 0) {
+            array[item.parent_id - 1].children = array[item.parent_id - 1].children !== undefined
+                ? [...array[item.parent_id - 1].children!, item]
+                : [item]
         }
-        return item
-    }).filter((item) => !duplicatedIds.some((id) => item.id === id))
+    })
+
+    return result
+
 }
